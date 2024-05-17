0b2725609e48bad2d96dcea718d4decc27cfc8f2
fzarco <filipezarcocoutinho@gmail.com>
2024-05-17 19:32:59 +0200

    refactor: converted Python2.x to Python3.x

--- Constants.py
+++ Constants.py
@@ -4,11 +4,11 @@ res = (SCREEN_WIDTH, SCREEN_HEIGHT)
 JUMP_VELOCITY = 15
 MAX_JUMP = 150
 
-MENU_START_X = SCREEN_WIDTH/4
-MENU_START_Y = SCREEN_HEIGHT/8
+MENU_START_X = SCREEN_WIDTH // 4
+MENU_START_Y = SCREEN_HEIGHT // 8
 
-ARROW_HALF_HEIGHT = 33 #px
-ARROW_HALF_WIDTH = 43 #px
+ARROW_HALF_HEIGHT = 33  # px
+ARROW_HALF_WIDTH = 43  # px
 
 GRAVITY = 1
 
@@ -16,4 +16,13 @@ GAME_CAPTION = 'Icy Tower Clone'
 
 ICE_RESISTANCE = 0.1
 
-ABOUT_MESSAGE = ["This game, clone of Icy Tower,", "was made by us, Dimitar", "and Stanimir with the purpose", "of practicing our Python and", "Pygame skills and", "participating in the", "Hack Together hackathon", "organized by Strypes"]
+ABOUT_MESSAGE = [
+    "This game, clone of Icy Tower,",
+    "was made by us, Dimitar",
+    "and Stanimir with the purpose",
+    "of practicing our Python and",
+    "Pygame skills and",
+    "participating in the",
+    "Hack Together hackathon",
+    "organized by Strypes"
+]
--- IceSprite.py
+++ IceSprite.py
@@ -1,16 +1,16 @@
-import pygame 
-from Utils import * 
+import pygame
+from Utils import *
 
 class IceSprite(pygame.sprite.Sprite):
-	image = None
+    image = None
 
-	def __init__(self, location):
-		pygame.sprite.Sprite.__init__(self)
+    def __init__(self, location):
+        super().__init__()
 
-		if IceSprite.image is None:
-			IceSprite.image = load_image("ice.png")
-		
-		self.image = IceSprite.image
+        if IceSprite.image is None:
+            IceSprite.image = load_image("ice.png")
 
-		self.rect = self.image.get_rect()
-		self.rect.topleft = location
+        self.image = IceSprite.image
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = location
